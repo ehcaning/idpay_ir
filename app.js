a4cf7c8afa308905bb5e928c85926c71b2a4b841
Ehsan <ehcan@icloud.com>
2020-06-03 09:49:02 +0430

    replace "request" with "axios"

--- app.js
+++ app.js
@@ -1,55 +1,62 @@
-const request = require('request');
+const axios = require('axios');
 
 class idpay {
-    constructor(api_key, sandbox = false) {
-        this.api_key = api_key;
-        this.sandbox = sandbox ? 1 : 0;
-    }
+	constructor(api_key, sandbox = false) {
+		this.api_key = api_key;
+		this.sandbox = sandbox ? 1 : 0;
+	}
 
-    create = (order_id, amount, callback, desc = '', name = '', phone = '', mail = '') => new Promise((resolve, reject) => {
-        var options = {
-            method: 'POST',
-            url: 'https://api.idpay.ir/v1.1/payment',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-API-KEY': this.api_key,
-                'X-SANDBOX': this.sandbox
-            },
-            body: {
-                'order_id': String(order_id),
-                'amount': parseInt(amount),
-                'name': name,
-                'phone': phone,
-                'mail': mail,
-                'desc': desc,
-                'callback': callback,
-                'reseller': null,
-            },
-            json: true,
-        };
+	create = (order_id, amount, callback, desc = '', name = '', phone = '', mail = '') =>
+		new Promise((resolve, reject) => {
+			axios({
+				method: 'post',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-API-KEY': this.api_key,
+					'X-SANDBOX': this.sandbox,
+				},
+				url: 'https://api.idpay.ir/v1.1/payment',
+				data: {
+					order_id: String(order_id),
+					amount: parseInt(amount),
+					name: name,
+					phone: phone,
+					mail: mail,
+					desc: desc,
+					callback: callback,
+					reseller: null,
+				},
+			})
+				.then(response => {
+					return resolve(response.data);
+				})
+				.catch(err => {
+					return reject(err.response.data);
+				});
+		});
 
-        request(options, (error, response, body) => resolve(body));
-    });
-
-    validate = (order_id, token) => new Promise((resolve, reject) => {
-        var options = {
-            method: 'POST',
-            url: 'https://api.idpay.ir/v1.1/payment/verify',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-API-KEY': this.api_key,
-                'X-SANDBOX': this.sandbox
-            },
-            body: {
-                'id': token,
-                'order_id': order_id,
-            },
-            json: true,
-        };
-
-        request(options, (error, response, body) => resolve(body));
-    });
+	validate = (order_id, token) =>
+		new Promise((resolve, reject) => {
+			axios({
+				method: 'post',
+				headers: {
+					'Content-Type': 'application/json',
+					'X-API-KEY': this.api_key,
+					'X-SANDBOX': this.sandbox,
+				},
+				url: 'https://api.idpay.ir/v1.1/payment/verify',
+				data: {
+					id: token,
+					order_id: order_id,
+				},
+			})
+				.then(res => {
+					return resolve(response.data);
+				})
+				.catch(err => {
+					return reject(err.response.data);
+				});
+		});
 }
-
 
 module.exports = (api_key, sandbox) => new idpay(api_key, sandbox);
